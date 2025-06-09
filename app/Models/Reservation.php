57dbd8509d6946ee5b7517c464cce57302023136
rortega8906 <raortega8906@gmail.com>
2025-06-09 23:53:56 +0200

    add relationship between User and Reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'reservation_date',
+        'reservation_time',
+        'status',
+        'notes'
+    ];
+
+    protected $casts = [
+        'reservation_date' => 'date',
+        'reservation_time' => 'datetime:H:i',
+        'status' => 'string'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
