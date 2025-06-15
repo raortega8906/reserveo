81887492fe73c8b06028220bb710f703fd5b821c
rortega8906 <raortega8906@gmail.com>
2025-06-15 22:26:49 +0200

    change footer information

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -170,44 +170,43 @@
 
                     <!-- Links -->
                     <div>
-                        <h3 class="text-sm font-semibold text-black uppercase tracking-wider">{{ __('Producto') }}</h3>
+                        <h3 class="text-sm font-semibold text-black uppercase tracking-wider">{{ __('Soporte') }}</h3>
                         <ul class="mt-4 space-y-2">
-                            <li><a href="#features" class="text-gray-600 hover:text-gray-900">{{ __('Características') }}</a></li>
-                            <li><a href="#" class="text-gray-600 hover:text-gray-900">{{ __('Precios') }}</a></li>
-                            <li><a href="#" class="text-gray-600 hover:text-gray-900">{{ __('Testimonios') }}</a></li>
+                            <li><a href="mailto:raortega8906@gmail.com" class="text-gray-600 hover:text-gray-900">{{ __('Contacto') }}</a></li>
+                            <li><a href="https://github.com/raortega8906/reserveo" class="text-gray-600 hover:text-gray-900">{{ __('Documentación') }}</a></li>
                         </ul>
                     </div>
 
                     <!-- Links -->
                     <div>
-                        <h3 class="text-sm font-semibold text-black uppercase tracking-wider">{{ __('Soporte') }}</h3>
+                        <h3 class="text-sm font-semibold text-black uppercase tracking-wider">{{ __('Producto') }}</h3>
                         <ul class="mt-4 space-y-2">
-                            <li><a href="#" class="text-gray-600 hover:text-gray-900">{{ __('Contacto') }}</a></li>
-                            <li><a href="#" class="text-gray-600 hover:text-gray-900">{{ __('Documentación') }}</a></li>
-                            <li><a href="#" class="text-gray-600 hover:text-gray-900">{{ __('Política de privacidad') }}</a></li>
+                            <li><a href="#features" class="text-gray-600 hover:text-gray-900">{{ __('Características') }}</a></li>
+                            {{-- <li><a href="#" class="text-gray-600 hover:text-gray-900">{{ __('Precios') }}</a></li> --}}
                         </ul>
                     </div>
+
                 </div>
 
                 <div class="mt-12 border-t border-gray-100 pt-8 flex flex-col md:flex-row justify-between items-center">
                     <p class="text-sm text-gray-500">
-                        &copy; {{ date('Y') }} {{ __('Reserveo. Todos los derechos reservados.') }}
+                        &copy; {{ date('Y') }} {{ __('Reserveo. Desarrollado por ') }}<a href="https://portfolio.wpcache.es" target="_blank" class="font-medium text-gray-900">{{ __('Rafael A. Ortega') }}</a>
                     </p>
                     <div class="mt-4 md:mt-0 flex space-x-6">
                         <!-- Social Links -->
-                        <a href="#" class="text-gray-500 hover:text-gray-900">
+                        <a href="https://github.com/raortega8906" class="text-gray-500 hover:text-gray-900">
                             <span class="sr-only">GitHub</span>
                             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
                             </svg>
                         </a>
-                        <a href="#" class="text-gray-500 hover:text-gray-900">
+                        <a href="https://www.linkedin.com/in/rafael-a-ortega/" class="text-gray-500 hover:text-gray-900">
                             <span class="sr-only">LinkedIn</span>
                             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452z" />
                             </svg>
                         </a>
-                        <a href="#" class="text-gray-500 hover:text-gray-900">
+                        <a href="mailto:raortega8906@gmail.com" class="text-gray-500 hover:text-gray-900">
                             <span class="sr-only">Gmail</span>
                             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
