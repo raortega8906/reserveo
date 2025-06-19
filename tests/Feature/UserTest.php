b47d47f1af334c5fd0759584b2bcf1c40b78fb6e
rortega8906 <raortega8906@gmail.com>
2025-06-20 00:25:55 +0200

    update UserTest

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,7 +1,62 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\User;
 
-    $response->assertStatus(200);
+use function Pest\Laravel\actingAs;
+
+test('Administrators can access the Reservations list', function () {
+
+    $admin = User::create([
+        'name' => 'Admin User',
+        'email' => 'admin@example.com',
+        'password' => bcrypt('password'),
+        'role' => 'admin',
+    ]);
+
+    actingAs($admin)
+        ->get('/admin/reservations')
+        ->assertStatus(200);
+});
+
+test('Clients cannot access the Reservations list', function () {
+    $client = User::create([
+        'name' => 'Client User',
+        'email' => 'client@example.com',
+        'password' => bcrypt('password'),
+        'role' => 'client',
+    ]);
+
+    actingAs($client)
+        ->get('/admin/reservations')
+        ->assertStatus(403);
+});
+
+test('Administrators can access the Services list', function () {
+
+    $admin = User::create([
+        'name' => 'Admin User',
+        'email' => 'admin@example.com',
+        'password' => bcrypt('password'),
+        'role' => 'admin',
+    ]);
+
+    actingAs($admin)
+        ->get('/admin/services')
+        ->assertStatus(200);
+
+});
+
+test('Clients cannot access the Services list', function () {
+
+    $client = User::create([
+        'name' => 'Client User',
+        'email' => 'client@example.com',
+        'password' => bcrypt('password'),
+        'role' => 'client',
+    ]);
+
+    actingAs($client)
+        ->get('/admin/services')
+        ->assertStatus(403);
+
 });
